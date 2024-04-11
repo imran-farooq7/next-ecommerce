4577fee125c21ec93bb93e43a8d5f4eefdef19d4
Imran Farooq <imran_farooq7@hotmail.com>
2024-04-11 16:55:44 +0500

    Register page completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import "bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
 import Navbar from "@/components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
 			<body className={inter.className}>
 				<Navbar />
 				{children}
+				<Toaster />
 			</body>
 		</html>
 	);
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,16 +1,32 @@
 "use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
+import toast from "react-hot-toast";
 
 const RegisterPage = () => {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
-	const [passwod, setPassword] = useState("");
+	const [password, setPassword] = useState("");
 	const [loading, setLoading] = useState(false);
+	const router = useRouter();
 
 	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		try {
 			setLoading(true);
+			const res = await axios.post("/api/register", { name, password, email });
+			if (res.status === 201) {
+				toast.success("user registration successfully");
+				router.push("/login");
+			} else {
+				toast.error("Something went wrong please try again later", {
+					position: "top-right",
+				});
+			}
+			setName("");
+			setPassword("");
+			setEmail("");
 		} catch (error) {
 			console.log(error);
 		} finally {
@@ -24,6 +40,35 @@ const RegisterPage = () => {
 				<div className="row vh-100 justify-content-center align-items-center">
 					<div className="col-lg-4 shadow bg-light p-4">
 						<h2 className="mb-4 text-center">Register</h2>
+						<form onSubmit={handleSubmit}>
+							<input
+								type="text"
+								value={name}
+								onChange={(e) => setName(e.target.value)}
+								className="form-control mb-3"
+								placeholder="Name"
+							/>
+							<input
+								type="email"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								className="form-control mb-3"
+								placeholder="Email"
+							/>
+							<input
+								type="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								className="form-control mb-3"
+								placeholder="Password"
+							/>
+							<button
+								className="btn btn-primary btn-raised d-block mx-auto w-100 rounded-4 mt-2"
+								disabled={loading || !name || !email || !password}
+							>
+								{loading ? "Please wait..." : "Register"}
+							</button>
+						</form>
 					</div>
 				</div>
 			</div>
