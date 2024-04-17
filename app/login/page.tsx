b3cc439638516c8979e588b62d58ba390e13d49c
Imran Farooq <imran_farooq7@hotmail.com>
2024-04-18 04:44:26 +0500

    Login page completed

---
+++ app/login/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
+
+const LoginPage = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [loading, setLoading] = useState(false);
+	const router = useRouter();
+
+	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		try {
+			setLoading(true);
+			const res = await signIn("credentials", {
+				email,
+				password,
+				redirect: false,
+			});
+			if (res?.error) {
+				toast.error(res.error);
+				setLoading(false);
+			} else {
+				toast.success("logged in successfully");
+				router.push("/");
+			}
+			setPassword("");
+			setEmail("");
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return (
+		<main>
+			<div className="container">
+				<div className="row vh-100 justify-content-center align-items-center">
+					<div className="col-lg-4 shadow bg-light p-4">
+						<h2 className="mb-4 text-center">Login</h2>
+						<form onSubmit={handleSubmit}>
+							<input
+								type="email"
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								className="form-control mb-3"
+								placeholder="Email"
+							/>
+							<input
+								type="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								className="form-control mb-3"
+								placeholder="Password"
+							/>
+							<button
+								className="btn btn-primary btn-raised d-block mx-auto w-100 rounded-4 mt-2"
+								disabled={loading || !email || !password}
+							>
+								{loading ? "Please wait..." : "Login"}
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
+};
+export default LoginPage;
