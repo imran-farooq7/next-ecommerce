fd456a3af4bec2cfbe5912e70d0e0fc93e1801cc
Imran Farooq <imran_farooq7@hotmail.com>
2024-04-23 04:23:34 +0500

    Loading page added

---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+const loading = () => {
+	return (
+		<div className="d-flex justify-content-center align-items-center vh-100 text-danger">
+			Loading...
+		</div>
+	);
+};
+export default loading;
