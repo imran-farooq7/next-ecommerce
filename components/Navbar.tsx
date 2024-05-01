d721f2eb844f0813b0527e40e828ea263f9678e3
Imran Farooq <imran_farooq7@hotmail.com>
2024-05-01 23:05:55 +0500

    Update user object with sesssion and jwt callbacks

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,15 +4,16 @@ import Link from "next/link";
 
 const Navbar = () => {
 	const { data, status } = useSession();
+	console.log(data);
 	return (
 		<nav className="nav shadow p-2 justify-content-between mb-2">
 			<Link href={"/"} className="nav-link">
 				🛒 NEXTECOMMERCE
 			</Link>
 			{status === "authenticated" ? (
-				<>
+				<div className="justify-content-end d-flex align-items-center">
 					<Link href={"/dashboard/user"} className="nav-link">
-						{data.user.name}
+						{data.user.name} ({data?.user?.role})
 					</Link>
 					<a
 						className="pointer"
@@ -20,7 +21,7 @@ const Navbar = () => {
 					>
 						Logout
 					</a>
-				</>
+				</div>
 			) : (
 				<div className="d-flex">
 					<Link href={"/login"} className="nav-link">
--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,9 +1,9 @@
 import User from "@/models/user";
 import brcypt from "bcrypt";
 import { AuthOptions } from "next-auth";
-import dbConnect from "./dbConntect";
 import CredentialsProvider from "next-auth/providers/credentials";
 import Google from "next-auth/providers/google";
+import dbConnect from "./dbConntect";
 export const authOptions: AuthOptions = {
 	session: {
 		strategy: "jwt",
@@ -23,7 +23,6 @@ export const authOptions: AuthOptions = {
 				},
 			},
 			async authorize(credentials, req) {
-				console.log(credentials, "auth options");
 				dbConnect();
 				const { email, password } = credentials;
 				const user = await User.findOne({ email });
@@ -39,6 +38,32 @@ export const authOptions: AuthOptions = {
 		}),
 	],
 	secret: process.env.NEXTAUTH_SECRET,
+	callbacks: {
+		async signIn({ user }) {
+			const { email } = user;
+			dbConnect();
+			const dbUser = await User.findOne({ email });
+			if (!dbUser) {
+				await User.create({
+					email,
+					name: user.name,
+					image: user.image,
+				});
+			}
+			return true;
+		},
+		async jwt({ token, user }) {
+			const userByEmail = await User.findOne({ email: token.email });
+			userByEmail.password = undefined;
+			userByEmail.resetCode = undefined;
+			token.user = userByEmail;
+			return token;
+		},
+		async session({ session, token }) {
+			session.user = token.user;
+			return session;
+		},
+	},
 	pages: {
 		signIn: "/login",
 	},
