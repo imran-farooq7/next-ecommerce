e06a08c2574b72dce0339563ebe7bdc9f4fedbc8
Imran Farooq <imran_farooq7@hotmail.com>
2024-07-05 20:46:23 +0500

    Tag context and provider completed

---
+++ context/TagContext.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { createContext, useState } from "react";
+import toast from "react-hot-toast";
+
+export const TagContext = createContext(null);
+
+export const TagProvider = ({ children }: { children: React.ReactNode }) => {
+	const [name, setName] = useState("");
+	const [parentCategory, setParentCategory] = useState("");
+	const [tags, setTags] = useState([]);
+	const [updatingTag, setUpdatingTag] = useState(null);
+	const createTag = async () => {
+		try {
+			const res = await fetch("/api/admin/tag", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					name,
+					parent: parentCategory,
+				}),
+			});
+			const data = await res.json();
+			if (res.ok) {
+				toast.success("Tag created successfully");
+				setName("");
+				setParentCategory("");
+				setTags([...tags, data?.tag]);
+			} else {
+				toast.error("Sorry tag could not be created");
+			}
+		} catch (error) {}
+	};
+	const getTag = async () => {
+		try {
+			const res = await fetch("/api/admin/tag", {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			const data = await res.json();
+			if (res.ok) {
+				setTags(data);
+			} else {
+				toast.error("Could not fetch tags");
+			}
+		} catch (error) {}
+	};
+	const updateTag = async () => {
+		try {
+			const res = await fetch(`/api/admin/tag/${updatingTag?._id}`, {
+				method: "PUT",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(updatingTag),
+			});
+			const data = await res.json();
+			if (res.ok) {
+				toast.success("Tag updated successfully");
+				setUpdatingTag(null);
+				setParentCategory("");
+				setTags((prevTag) => {
+					return prevTag?.map((tag) => (tag?._id === data?._id ? data : tag));
+				});
+			} else {
+				toast.error("Sorry tag could not be updated");
+			}
+		} catch (error) {}
+	};
+	const deleteTag = async () => {
+		try {
+			const res = await fetch(`/api/admin/tag/${updatingTag?._id}`, {
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			const data = await res.json();
+			if (res.ok) {
+				toast.success("tag deleted successfully");
+				setTags((prevTag) => {
+					return prevTag?.filter((tag) => tag?._id !== data?._id);
+				});
+			}
+		} catch (error) {}
+	};
+	return (
+		<TagContext.Provider
+			value={{
+				name,
+				setName,
+				parentCategory,
+				setParentCategory,
+				tags,
+				setTags,
+				updatingTag,
+				setUpdatingTag,
+				createTag,
+				getTag,
+				updateTag,
+				deleteTag,
+			}}
+		>
+			{children}
+		</TagContext.Provider>
+	);
+};
